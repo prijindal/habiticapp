6623b45e5bcb980a5d571cca0418956797c2021b
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-03-03 19:22:31 +0530

    Added Markdown Support in text

--- lib/screens/home/task.dart
+++ lib/screens/home/task.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../models/task.dart';
+import '../../libraries/markdown/flutter_markdown.dart';
+import '../../helpers/markdown.dart';
 
 class TaskContainer extends StatelessWidget {
   TaskContainer({Key key, this.task}):super(key: key);
@@ -12,7 +14,9 @@ class TaskContainer extends StatelessWidget {
       // TODO: implement build
       return new Container(
         child: new ListTile(
-          title: new Text(task.text)
+          title: new MarkdownBody(
+            data: convertEmojis(task.text),
+          )
         )
       );
     }
