75c463670b2735cd7af5429302b957ad7219ec92
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-03-06 02:29:43 +0530

    Fixed release command

--- ci/script.sh
+++ ci/script.sh
@@ -4,25 +4,25 @@ set -ex
 GITHUB_RELEASE_VERSION="0.7.2"
 USER_NAME="prijindal"
 REPO_NAME="habiticapp"
-TAG_NAME="v0.0.1"
+TAG_NAME="auto-build"
 
 
 export PATH="$PWD/flutter/bin:$PWD/flutter/bin/cache/dart-sdk/bin:$PATH"
 
 if [ "$SHARD" = "build_and_deploy" ]; then
   echo "Building Application"
+  wget "https://github.com/aktau/github-release/releases/download/v$GITHUB_RELEASE_VERSION/linux-amd64-github-release.tar.bz2"
+  mkdir github-release
+  tar xvjf linux-amd64-github-release.tar.bz2 -C github-release
   if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     export ANDROID_HOME=`pwd`/android-sdk
     flutter build apk --release
     echo "Android Application built"
-    echo "Deploying to Github"
-    wget "https://github.com/aktau/github-release/releases/download/v$GITHUB_RELEASE_VERSION/linux-amd64-github-release.tar.bz2"
-    mkdir github-release
-    tar xvjf linux-amd64-github-release.tar.bz2 -C github-release
+    echo "Deploying Android application to Github"
     # ./github-release/bin/linux/amd64/github-release delete -u $USER_NAME -r $REPO_NAME --tag "v0.0.1"
     # ./github-release/bin/linux/amd64/github-release release -u $USER_NAME -r $REPO_NAME --tag "v0.0.1" -p --name "Pre Release $(date)"
-    ./github-release/bin/linux/amd64/github-release upload -u $USER_NAME -r $REPO_NAME --tag "v0.0.1" --name "app-release" --file build/app/outputs/apk/release/app-release.apk -R
-    echo "Deployed Succesfully"
+    ./github-release/bin/linux/amd64/github-release upload -u $USER_NAME -r $REPO_NAME --tag $TAG_NAME --name "app-release.apk" --file build/app/outputs/apk/release/app-release.apk -R
+    echo "Android Deploy Successfull"
     # Add to github release
   elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "Building Application for iOS..."
