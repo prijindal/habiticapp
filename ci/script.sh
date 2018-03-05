44a895a136d7bece9b6df88fab23f05157ee7047
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-03-05 23:33:27 +0530

    Fixed flutter build ios command in script.sh

--- ci/script.sh
+++ ci/script.sh
@@ -7,11 +7,11 @@ if [ "$SHARD" = "build_and_deploy" ]; then
   echo "Building Application"
   if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     export ANDROID_HOME=`pwd`/android-sdk
-    ./flutter/bin/flutter build apk --release
+    flutter build apk --release
     echo "Android Application built"
   elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "Building Application for iOS..."
-    flutter build ios --release -no-codesign
+    flutter build ios --release --no-codesign
     echo "iOS Flutter Gallery built"
   fi
 else
