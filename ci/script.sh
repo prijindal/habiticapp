310e2387f44ccaa0f63d2f7a327de3d28fa2dadc
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-03-06 02:45:31 +0530

    Added Ios Github release command

--- ci/script.sh
+++ ci/script.sh
@@ -11,23 +11,33 @@ export PATH="$PWD/flutter/bin:$PWD/flutter/bin/cache/dart-sdk/bin:$PATH"
 
 if [ "$SHARD" = "build_and_deploy" ]; then
   echo "Building Application"
-  wget "https://github.com/aktau/github-release/releases/download/v$GITHUB_RELEASE_VERSION/linux-amd64-github-release.tar.bz2"
-  mkdir github-release
-  tar xvjf linux-amd64-github-release.tar.bz2 -C github-release
   if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     export ANDROID_HOME=`pwd`/android-sdk
     flutter build apk --release
     echo "Android Application built"
+
     echo "Deploying Android application to Github"
-    # ./github-release/bin/linux/amd64/github-release delete -u $USER_NAME -r $REPO_NAME --tag "v0.0.1"
-    # ./github-release/bin/linux/amd64/github-release release -u $USER_NAME -r $REPO_NAME --tag "v0.0.1" -p --name "Pre Release $(date)"
+
+    wget "https://github.com/aktau/github-release/releases/download/v$GITHUB_RELEASE_VERSION/linux-amd64-github-release.tar.bz2"
+    mkdir github-release
+    tar xvjf linux-amd64-github-release.tar.bz2 -C github-release
+
     ./github-release/bin/linux/amd64/github-release upload -u $USER_NAME -r $REPO_NAME --tag $TAG_NAME --name "app-release.apk" --file build/app/outputs/apk/release/app-release.apk -R
+    ./github-release/bin/linux/amd64/github-release edit -u $USER_NAME -r $REPO_NAME --tag $TAG_NAME --name "Pre Release $(date)"
     echo "Android Deploy Successfull"
-    # Add to github release
+
   elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     echo "Building Application for iOS..."
     flutter build ios --release --no-codesign
     echo "iOS Application built"
+
+    echo "Deploying Ios application to Github"
+    wget "https://github.com/aktau/github-release/releases/download/v$GITHUB_RELEASE_VERSION/darwin-amd64-github-release.tar.bz2"
+    mkdir github-release
+    tar xvjf darwin-amd64-github-release.tar.bz2 -C github-release
+    ./github-release/bin/linux/amd64/github-release upload -u $USER_NAME -r $REPO_NAME --tag $TAG_NAME --name "Runner.app" --file build/ios/iphoneos/Runner.app -R
+    echo "Ios Deploy Successfull"
+
   fi
 else
   flutter packages get
