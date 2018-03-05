00079f37ffcbfe1453fa1e841a4056c016705e50
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-03-06 02:04:08 +0530

    Added Github release upload command

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,10 @@
 set -ex
 
 GITHUB_RELEASE_VERSION="0.7.2"
+USER_NAME="prijindal"
+REPO_NAME="habiticapp"
+TAG_NAME="v0.0.1"
+
 
 export PATH="$PWD/flutter/bin:$PWD/flutter/bin/cache/dart-sdk/bin:$PATH"
 
@@ -15,7 +19,9 @@ if [ "$SHARD" = "build_and_deploy" ]; then
     wget "https://github.com/aktau/github-release/releases/download/v$GITHUB_RELEASE_VERSION/linux-amd64-github-release.tar.bz2"
     mkdir github-release
     tar xvjf linux-amd64-github-release.tar.bz2 -C github-release
-    ./github-release/bin/linux/amd64/github-release
+    # ./github-release/bin/linux/amd64/github-release delete -u $USER_NAME -r $REPO_NAME --tag "v0.0.1"
+    # ./github-release/bin/linux/amd64/github-release release -u $USER_NAME -r $REPO_NAME --tag "v0.0.1" -p --name "Pre Release $(date)"
+    ./github-release/bin/linux/amd64/github-release upload -u $USER_NAME -r $REPO_NAME --tag "v0.0.1" --name "app-release" --file build/app/outputs/apk/release/app-release.apk -R
     echo "Deployed Succesfully"
     # Add to github release
   elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
